4680a766fc6cb9ab059026f1ca7d324f92011ccc
TheCloudWorld <74950445+TheCloudWorldAdmin@users.noreply.github.com>
2023-03-27 12:28:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+  # The security credentials for AWS Account A.
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = "us-east-1"
+
+  assume_role {
+    # roles of account B where resources will be deloyed
+    role_arn    = "arn:aws:iam::569775151562:role/terraform-access"
+    external_id = "abcdef"
+  }
 }
